7634b24ea11d6fb31c66a138b937ad79beef6f90
Jonathan Paul <netsky@kubernetes.main>
2023-02-13 14:50:31 +0100

    removed unneeded functions

--- python/removeBattery/removeBattery.py
+++ python/removeBattery/removeBattery.py
@@ -6,9 +6,7 @@ try:
   login = api.login('E-MAIL', 'PASSWORD')
   '''In order to delete a battery you need to get the "id" of the require battery
      one can do that copying the id from the Service Center or querying your battery and get its id'''
-  batterie = api.get_batteries(filter={"name": "NAME"}, option= {"limit": 100})[0]
-  print(batterie["id"])
-  removed_battery = api.remove_battery_by_id(batterie["id"])
+  removed_battery = api.remove_battery_by_id("BATTERY_ID")
 
 
 except:
